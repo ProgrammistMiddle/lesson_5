f56cd436dda2c9c2970dfb119a7a39beb2f96afc
ProgrammistMiddle <59846651+ProgrammistMiddle@users.noreply.github.com>
2020-01-17 12:51:21 +0400

    Update lesson_5.py

--- lesson_5.py
+++ lesson_5.py
@@ -13,6 +13,7 @@ print(prime(17))
 print('2) функция выводит список всех делителей числа:', 256)
 def denominator(n):
     result = []
+    n = n // 2
     for i in range(1,n+1):
         if n % i == 0:
             result.append(i)
